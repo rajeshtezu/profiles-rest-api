2865c078ae5c7c9a929a9c1dc42477fe8892cad0
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-10-23 12:43:52 +0530

    added put, patch, delete method for testing APIView

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -37,3 +37,21 @@ class HelloApiView(APIView):
 
         else:
             return Response(serializer.errors ,status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self, request, pk=None):
+        """Handles updating an object."""
+
+        return Response({'method':'put'})
+
+
+    def patch(self, request, pk=None):
+        """Patch request, only updates fields provided in the request."""
+
+        return Response({'method':'patch'})
+
+
+    def delete(self, request, pk=None):
+        """Deletes an object."""
+
+        return Response({'method':'delete'})
